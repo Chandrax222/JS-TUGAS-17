8ac5a0a4d165dcfdb59a9683292cf04cdeb1f8a9
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-25 10:44:11 +0700

    ChartCode
    ChartCode

---
+++ Tugas17_2.js
@@ -0,0 +1,13 @@
+function Tugas17_2() {
+    var kode = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, hic inventore voluptatum quaerat omnis porro, non debitis voluptas voluptate totam ad facere facilis in, temporibus blanditiis nam fuga et error!";
+
+    console.log(kode.charCodeAt(0)); //76
+    console.log(kode.charCodeAt(1)); //111
+    console.log(kode.charCodeAt(2)); //114
+    console.log(kode.charCodeAt(3)); //101
+    console.log(kode.charCodeAt(4)); //109
+    console.log(kode.charCodeAt(5)); //32
+
+}
+Tugas17_2();
+
