60be2e74223fb8c48a074808f71103cdd3eb1f8e
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-25 10:43:50 +0700

    tolowerCase dan toUpperCase
    tolowerCase dan toUpperCase

---
+++ Tugas17_1.js
@@ -0,0 +1,8 @@
+function Tugas17_1() {
+    var pertama = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var kedua = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+    console.log(pertama.toLowerCase());
+    console.log(kedua.toUpperCase());
+}
+Tugas17_1();
